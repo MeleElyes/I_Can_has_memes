1eccea4761a47ce71d548d49b73584faa501a7ba
Elyes MELE <mele.elyes@gmail.com>
2023-01-10 15:58:38 +0100

    done

--- src/Component/Meme.js
+++ src/Component/Meme.js
@@ -1,11 +1,17 @@
+import Draggable from "react-draggable";
+
 const Meme = (props) => {
 
   return (
     <div className="memedisplay">
     <div className="meme">
+    <Draggable bounds="parent">
       <h2 className="meme--text top">{props.textTop}</h2>
+      </Draggable>
       <img className="meme--image" src={props.randommeme} width={"400px"} alt="meme" />
+      <Draggable bounds="parent">
       <h2 className="meme--text bottom">{props.textBottom}</h2>
+      </Draggable>
     </div>
     </div>
   );
